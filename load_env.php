7d25f4242c088cdc95e9739d56c3b2c758e5ae3a
hiro <48009332+ehirokatsu@users.noreply.github.com>
2025-02-18 14:38:29 +0900

    renderとローカルの区別化

--- load_env.php
+++ load_env.php
@@ -1,9 +1,12 @@
 <?php
-if (file_exists(__DIR__ . '/.env')) {
-    $lines = file(__DIR__ . '/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) continue; // コメント行を無視
-        putenv($line);
+// Render 環境では `getenv()` を使うので、.env の読み込みは不要
+if (!getenv('RENDER')) { 
+    if (file_exists(__DIR__ . '/.env')) {
+        $lines = file(__DIR__ . '/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) continue; // コメント行を無視
+            putenv($line);
+        }
     }
 }
 ?>
